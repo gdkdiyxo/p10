02b68c7e42d7a6af2585a11f8685acd91ff76a68
ATengelics <Baychuks@gmail.com>
2019-11-17 13:31:32 -0500

    React src Folder
    Source files for the React App. /src

---
+++ reactSource/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+
+function MyButton(){
+  return (<button>Click</button>);
+  }
+
+  function MyLogo(){
+    return(<h1>Rutgers Internship Service</h1>);
+  }
+
+class Navbar extends React.Component{
+  render() {
+      return (
+          <div>
+            <ul id="nav">
+              <li><a href="#">Find Jobs</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Sign Up</a></li>
+              <li><a href="#">Log in</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
+      );
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <MyLogo />
+      <MyButton />
+    </div>
+  );
+}
+
+export default App;
