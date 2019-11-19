cb7e0e7c8a3ac13ec9ddb8c60c37724b2571522d
ATengelics <Baychuks@gmail.com>
2019-11-19 11:59:15 -0500

    added mock contact
    3 links now work in the nav bar. still need sign up and about

--- reactSource/App.js
+++ reactSource/App.js
@@ -14,11 +14,11 @@ class Navbar extends React.Component{
       return (
           <div>
             <ul id="nav">
-              <li><a href="#">Find Jobs</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Sign Up</a></li>
-              <li><a href="https://navpagemocklogin.s3.us-east-2.amazonaws.com/mocklogin.html">Log in</a></li>
-              <li><a href="#">Contact</a></li>
+            <li><a href="https://cs491project.s3.us-east-2.amazonaws.com/index.html">Find Jobs</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Sign Up</a></li>
+            <li><a href="https://navpagemocklogin.s3.us-east-2.amazonaws.com/mocklogin.html">Log in</a></li>
+            <li><a href="https://navpagemockcontact.s3.us-east-2.amazonaws.com/mockcontact.html">Contact</a></li>
             </ul>
           </div>
       );
