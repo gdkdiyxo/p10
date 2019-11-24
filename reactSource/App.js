6b5f55efde6c2aa2e73233a6e5d40897b236aa82
davidballad <davicho_balla@hotmail.com>
2019-11-24 11:17:53 -0500

    updated App.js with external components

--- reactSource/App.js
+++ reactSource/App.js
@@ -1,38 +1,24 @@
+//jshint esversion: 6
+
 import React from 'react';
 import './App.css';
+import Heading from "./components/Heading";
+import NavBar from "./components/NavBar";
+import SearchTool from "./components/SearchTool";
+import Table from "./components/Table";
 
-function MyButton(){
-  return (<button>Click</button>);
-  }
-
-  function MyLogo(){
-    return(<h1>Rutgers Internship Service</h1>);
-  }
-
-class Navbar extends React.Component{
-  render() {
-      return (
-          <div>
-            <ul id="nav">
-            <li><a href="https://cs491project.s3.us-east-2.amazonaws.com/index.html">Find Jobs</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Sign Up</a></li>
-            <li><a href="https://navpagemocklogin.s3.us-east-2.amazonaws.com/mocklogin.html">Log in</a></li>
-            <li><a href="https://navpagemockcontact.s3.us-east-2.amazonaws.com/mockcontact.html">Contact</a></li>
-            </ul>
-          </div>
-      );
-  }
-}
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <MyLogo />
-      <MyButton />
+      <NavBar />
+      <Heading />
+      <SearchTool />
+      <Table />
     </div>
   );
 }
+
+
 
 export default App;
